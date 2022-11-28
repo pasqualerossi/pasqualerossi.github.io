cc97bbf966d2bb9949d191655c9069e0269af7e5
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-11-28 14:13:55 +1030

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -6,16 +6,16 @@ using namespace std;
 int main()
 {
     string line;
-    ifstream myfile ("gamefeed.xml");
+    ifstream myfile ("games.txt");
     if (myfile.is_open())
     {
         while (getline(myfile,line))
 		{
-			if (line.find("<url>") != string::npos && line.find("</url>") != string::npos)
+			if (line.find("href=") != string::npos && line.find("title") != string::npos)
 			{
-				for (int i = 0; i < 11; i++)
+				for (int i = 0; i < 7; i++)
 					line.erase(line.begin());
-				for (int i = 0; i < 6; i++)
+				for (int i = 0; i < 2; i++)
 					line.erase(line.begin() + line.size() - 1);
 				cout << "\"" << line << "\"," << endl;
 			}
